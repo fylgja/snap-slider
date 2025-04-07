c07e240703e51a3c7cb7fa68fe1d8a0fd47e1b2f
grimlink <sean.grimlink@gmail.com>
2025-04-07 22:33:30 +0200

    FIX: marker group render

--- src/snap-slider.js
+++ src/snap-slider.js
@@ -1,6 +1,9 @@
 const SnapSlider = class extends HTMLElement {
     constructor() {
         super();
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.innerHTML =
+            "<style>:host{display:block}</style><slot></slot>";
         this.track = null;
         this.pager = null;
         this.slides = [];
